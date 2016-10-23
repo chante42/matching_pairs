80d734a31c8d8997c15e7ebb1076b89a6721b272
Olivier Chanteloup <chante42@gmail.com>
2016-10-23 09:24:34 +0200

    new file:   fondScore.png 	modified:   index.html 	modified:   matching pairs.js 	new file:   phaser.min.js 	modified:   phaser_tiles.png
     gestion correct, avec un affichage des carreux cacher avec 2 a 8 ligne
     colonnes

--- matching pairs.js
+++ matching pairs.js
@@ -2,19 +2,15 @@
 // twitter: @pato_reilly
 
 var tileBack = 63;
+var tileBlack = 32;
 var NbCaseRemplie = 26*2;
-var LargeurJeux = 4;
+var NbCase = 64;
+var LargeurJeux = 3;
 var HauteurJeux = LargeurJeux;
 
 
 var game = new Phaser.Game(LargeurJeux *100 +210 , LargeurJeux * 100, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
-function preload() {
-
-    game.load.tilemap('matching', 'phaser_tiles.json', null, Phaser.Tilemap.TILED_JSON);
-    
-    game.load.image('tiles', 'phaser_tiles.png', LargeurJeux * 100 , HauteurJeux * 100, -1, 1, 1);    
-}
 
 var timeCheck = 0;
 var flipFlag = false;
@@ -45,32 +41,88 @@ var youWin = '+';
 
 var myCountdownSeconds;
 
+//
+//  preload
+//
+function preload() {
 
+    game.load.tilemap('matching', 'phaser_tiles.json', null, Phaser.Tilemap.TILED_JSON);
+    
+    game.load.image('tiles', 'phaser_tiles.png');
+    game.load.image('fondScore', 'fondScore.png');
+}
+
+
+//
+//  create
+//
 function create() {
 
-        map = game.add.tilemap('matching');
-        
-        map.addTilesetImage('Desert', 'tiles');
+    map = game.add.tilemap('matching');
 
-        //tileset = game.add.tileset('tiles');
-    
-        layer = map.createLayer('Ground');//.tilemapLayer(0, 0, 600, 600, tileset, map, 0);
 
-        //layer.resizeWorld();
+    map.addTilesetImage('Desert', 'tiles');
 
-        marker = game.add.graphics();
-        marker.lineStyle(2, 0xFF0000, 1);
-        marker.drawRect(0, 0, 100, 100);
-    
+    //tileset = game.add.tileset('tiles');
+
+    layer = map.createLayer('Ground');//.tilemapLayer(0, 0, 600, 600, tileset, map, 0);
+
+    //layer.resizeWorld();
+
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0xFF0000, 1);
+    marker.drawRect(0, 0, 100, 100);
+    marker.x = 0;
+    marker.y = 0;
+
     randomizeTiles();
+
+    this.leftKey   = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    this.rightKey  = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    this.downKey   = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    this.upKey     = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    this.spaceKey  = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    //  Stop the following keys from propagating up to the browser
+    game.input.keyboard.addKeyCapture([ Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, 
+        Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT, Phaser.Keyboard.SPACEBAR ]);
+    
+    fondScore = game.add.tileSprite(LargeurJeux* 100, 0, 210 , 800,'fondScore');
     //debug
     //flipOverAll();
-
 }
 
 function update() {
     
     countDownTimer();
+ 
+    if (this.leftKey.isDown)
+    {
+        marker.x -= 100;
+        if (marker.x < 0) 
+            { marker.x=0;}
+    }
+    else if (this.rightKey.isDown)
+    {
+        marker.x += 100;
+        if (marker.x > (LargeurJeux - 1) * 100)
+            { marker.x=(LargeurJeux - 1)*100;}
+    }
+
+    if (this.upKey.isDown)
+    {
+        marker.y -= 100;
+        if (marker.y < 0) 
+            { marker.y=0;}
+    }
+    else if (this.downKey.isDown)
+    {
+        marker.y += 100;
+        if (marker.y > (HauteurJeux -1) *  100) 
+            { marker.y=(HauteurJeux - 1) *100;}
+        console.log("DOWN X:"+marker.x+" Y:"+marker.y);
+    }
+
     
     if (layer.getTileX(game.input.activePointer.worldX) <= LargeurJeux - 1) // to prevent the marker from going out of bounds
     {
@@ -114,10 +166,9 @@ function processClick() {
     currentTile = map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y));
     currentTilePosition = ((layer.getTileY(game.input.activePointer.worldY)+1)*LargeurJeux)-(LargeurJeux-(layer.getTileX(game.input.activePointer.worldX)+1));
         
-    if (game.input.mousePointer.isDown)
-        {
+    if (game.input.mousePointer.isDown ){
         // check to make sure the tile is not already flipped
-        if (currentTile.index == tileBack)
+        if (currentTile != null && currentTile.index == tileBack)
         {
             // get the corresponding item out of squareList
                 currentNum = squareList[currentTilePosition-1];
@@ -134,7 +185,7 @@ function processClick() {
                 {
                     masterCounter++;    
                     
-                    if (masterCounter == (LargeurJeux*HauteurJeux/2) ) {
+                    if (masterCounter == Math.trunc(LargeurJeux*HauteurJeux/2) ) {
                         // go "win"
                         youWin = 'Got them all!';
                     }                       
@@ -158,7 +209,6 @@ function processClick() {
 }
  
 function flipOver() {
- 
     map.putTile(currentNum, layer.getTileX(marker.x), layer.getTileY(marker.y));
 }
  
@@ -170,8 +220,6 @@ function flipOverAll() {
         map.putTile(squareList[i], x , y );    
         console.log(i + ":x="+x+"y="+y);
     }
-    
-
 }
 
 function flipBack() {
@@ -184,8 +232,7 @@ function flipBack() {
 }
  
 function randomizeTiles() {
-
-        //
+     //
     // s'il reste des trous tirage aléatoire en mettaunt une carte de chaque 32=>x>0 et 64=>x>32
     nbItem = startList.length;
     console.log('nbItem:'+nbItem+" max for  "+HauteurJeux*LargeurJeux);
@@ -225,14 +272,21 @@ function randomizeTiles() {
     console.log('startList'+startList.toString());
   
     for (col = 0; col < HauteurJeux; col++) {
-        for (row = 0; row < LargeurJeux; row++) {
+        for (row = 0; row < LargeurJeux ; row++) {
             map.putTile(tileBack, col, row);
         }
     }
+
+    // met les autres carreaux ('tiles') en noirs.
+    for (col = LargeurJeux ; col < NbCase / 2 ; col++) {
+        for (row = 0 ; row < NbCase / 2 ; row++) {
+            map.putTile(tileBlack, col, row);
+        }
+    }
+
 }
 
 function getHiddenTile() {
-        
     thisTile = squareList[currentTilePosition-1];
     return thisTile;
 }
@@ -240,7 +294,7 @@ function getHiddenTile() {
 function render() {
 
 var posx = LargeurJeux *100 +20;
-    game.debug.text('LargeurJeux : '+LargeurJeux, posx, 40, 'rgb(255,0,0)');
+    //game.debug.text('LargeurJeux : '+LargeurJeux, posx, 40, 'rgb(255,0,0)');
     game.debug.text(timesUp, posx, 208, 'rgb(0,255,0)');
     game.debug.text(youWin, posx, 240, 'rgb(0,255,0)');
 
@@ -255,9 +309,9 @@ var posx = LargeurJeux *100 +20;
 
     //game.debug.text('Tile: ' + map.getTile(layer.getTileX(marker.x), layer.getTileY(marker.y)).index, 620, 48, 'rgb(255,0,0)');
 
-    game.debug.text('LayerX: ' + layer.getTileX(marker.x), posx, 80, 'rgb(255,0,0)');
-    game.debug.text('LayerY: ' + layer.getTileY(marker.y), posx, 112, 'rgb(255,0,0)');
+    //game.debug.text('LayerX: ' + layer.getTileX(marker.x), posx, 80, 'rgb(255,0,0)');
+    //game.debug.text('LayerY: ' + layer.getTileY(marker.y), posx, 112, 'rgb(255,0,0)');
 
-    game.debug.text('Tile Position: ' + currentTilePosition, posx, 144, 'rgb(255,0,0)');
-    game.debug.text('Hidden Tile: ' + getHiddenTile(), posx, 176, 'rgb(255,0,0)');
+    //game.debug.text('Tile Position: ' + currentTilePosition, posx, 144, 'rgb(255,0,0)');
+    //game.debug.text('Hidden Tile: ' + getHiddenTile(), posx, 176, 'rgb(255,0,0)');
 }
